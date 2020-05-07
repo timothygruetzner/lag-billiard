542accdba5835424e4a336ae37bbe4db995faf10
Timothy Gruetzner <timothy.gruetzner@students.fhnw.ch>
2020-05-07 09:25:37 +0200

    Split wall into own class

--- billard.py
+++ billard.py
@@ -1,18 +1,11 @@
 from tkinter import *
-
-
-class Wall:
-    def __init__(self):
-        self.x_start = 0
-        self.y_start = 0
-        self.x_end = 0
-        self.y_end = 0
-        self.line_id = 0
+from wall import Wall
 
 window = Tk()
 window.geometry("600x900")
 wallCounter = 0
 walls = []
+
 
 def pressed(event):
     walls.append(Wall())
@@ -25,6 +18,7 @@ def moved(event):
         c.delete(walls[wallCounter].line_id)
     walls[wallCounter].line_id = c.create_line(walls[wallCounter].x_start, walls[wallCounter].y_start, event.x, event.y)
 
+
 def released(event):
     global wallCounter
     walls[wallCounter].x_end = event.x
@@ -32,10 +26,10 @@ def released(event):
     wallCounter += 1
 
 
-window.bind("<Button-1>", pressed)
-window.bind("<B1-Motion>", moved)
-window.bind("<ButtonRelease-1>", released)
+window.bind("<Button-3>", pressed)
+window.bind("<B3-Motion>", moved)
+window.bind("<ButtonRelease-3>", released)
 
-c = Canvas(window, width = 600, height=900)
+c = Canvas(window, width=600, height=900)
 c.pack()
 window.mainloop()
---
+++ wall.py
@@ -0,0 +1,7 @@
+class Wall:
+    def __init__(self):
+        self.x_start = 0
+        self.y_start = 0
+        self.x_end = 0
+        self.y_end = 0
+        self.line_id = 0
